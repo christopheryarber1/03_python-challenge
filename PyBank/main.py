0d18c489191383ea0f0d0a8fa97a8ba9aedd56c6
unknown <christopheryarber1@gmail.com>
2022-05-06 14:03:54 -0400

    Added folders and files per instructions. Created two python files with two output text files.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,112 @@
-print ("Hello World! PyBank here!")
+
+# Imports Listing
+import os
+import csv
+import numpy as np
+
+#filepath where csv file resides
+csvfilepath = 'C:\GitRepos\python-challenge\PyBank\Resources\Budget_data.csv'
+
+
+# Set up lists to store CSV Data split out
+Date = []
+ProfitLosses = []
+
+with open(csvfilepath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    #print(csvreader)
+
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    # Read each row of data after the header
+    for row in csvreader:
+        #print(row)
+
+        # Add Date Data
+        Date.append(row[0])
+
+        # Add Profit Loss Data
+        ProfitLosses.append(row[1])
+
+# Review Split out Lists
+print(Date)
+print(" ")    
+print(ProfitLosses)
+print(" ")
+print(" ")
+
+#Convert List to Integers
+ProfitLosses = list(map(int, ProfitLosses))
+
+
+######################
+# Calculate Values
+######################
+
+#Find Count of Total Months - GOOD 
+CountMonths = len(Date)
+
+#Find Net Profit/Loss  - GOOD
+TotalProfit = sum(ProfitLosses)
+
+#Cacluate change in Profit/Loss for entire Period - GOOD 
+  # Calculating change in profits month-to-month
+Change_Profits = []
+for x, y in zip(ProfitLosses[0::], ProfitLosses[1::]):
+    Change_Profits.append(y-x)
+      
+# printing change in profits month-to-month
+#print ("Change_Profits: ", str(Change_Profits))
+
+#Change in Profits Month to month - GOOD
+ChangeInProfit = sum(Change_Profits)
+
+#Calculate Average Change in Profit/Loss - GOOD
+AvgProfit = str(round((ChangeInProfit/(CountMonths-1)), 2))
+
+#Identify Greatest Profit Increase  - GOOD
+MaxProfit = max(ProfitLosses)
+
+#Identify Greatest Profit Decrease  - GOOD
+MinProfit = min(ProfitLosses)
+
+
+######################
+# Print To Terminal
+######################
+#Print Variables to Terminal 
+print(f"Total Months: {CountMonths}")
+print(f"Net Total Profit: {TotalProfit}")
+print(f"Average Change: {AvgProfit}")
+print(f"Greatest Increase In Profits: {MaxProfit}")
+print(f"Greatest Decrease In Profits: {MinProfit}")
+print(" ")
+print(" ")
+
+
+######################
+# Generate Text File
+######################
+
+#Setting Filepath manually due to issues 
+#print(os.path.abspath(os.getcwd()))
+currentpath=(os.path.abspath(os.getcwd()))
+resultfilepath=(currentpath+'\Resources\Results.txt')
+print(f"Writing file to following location: {resultfilepath} ")
+
+f = open(resultfilepath, 'w')
+f.write(f"Total Months: {CountMonths}")
+f.write('\n')
+f.write(f"Net Total Profit: {TotalProfit}")
+f.write('\n')
+f.write(f"Average Change: {AvgProfit}")
+f.write('\n')
+f.write(f"Greatest Increase In Profits: {MaxProfit}")
+f.write('\n')
+f.write(f"Greatest Decrease In Profits: {MinProfit}")
+f.close()
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,145 @@
-print ("Hello World! PyPoll Here!")
+
+# Imports Listing
+import os
+import csv
+import numpy as np
+
+#filepath where csv file resides
+csvfilepath = 'C:\GitRepos\python-challenge\PyPoll\Resources\election_data.csv'
+
+
+
+# Set up lists to store CSV Data split out
+VoterIDList = []
+CountyList = []
+CandidateList = []
+
+with open(csvfilepath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    #print(csvreader)
+
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    #print(f"CSV Header: {csv_header}")
+
+    # Read each row of data after the header
+    for row in csvreader:
+        #print(row)
+
+         # Add VoterID
+         VoterIDList.append(row[0])
+
+         # Add County
+         CountyList.append(row[1])
+
+         # Add County
+         CandidateList.append(row[2])    
+
+# # Review Split out Lists
+# print(VoterIDList)
+# print(" ")    
+# print(CountyList)
+# print(" ")
+# print(CandidateList)
+# print(" ")
+# print(" ")
+
+# #Convert List to Integers
+# ProfitLosses = list(map(int, ProfitLosses))
+
+
+######################
+# Calculate Values
+######################
+
+#Find Count of Total Votes - GOOD 
+CountofVotes = len(VoterIDList)
+
+#Find Distinct Candidates in Candidate List  - GOOD
+DistinctCandidateList = []
+for x in CandidateList:
+    if x not in DistinctCandidateList:
+        DistinctCandidateList.append(x)
+#print(DistinctCandidateList)
+
+
+#Cacluate Total Votes for each candidate - GOOD 
+KhanVotes = CandidateList.count('Khan')
+CorreyVotes = CandidateList.count('Correy')
+LiVotes = CandidateList.count('Li')
+OTooleyVotes = CandidateList.count("O'Tooley")
+
+CandidateVoteCounts = [KhanVotes,CorreyVotes,LiVotes,OTooleyVotes]
+
+# #Identify Highest Vote Count  - GOOD
+HighestVoteCount = max(CandidateVoteCounts)
+
+WinnerListing = {'Khan': KhanVotes, 'Correy': CorreyVotes, 'Li': LiVotes, 'OTooley': OTooleyVotes}
+winner = max(WinnerListing, key=WinnerListing.get)
+print(winner, 'won')
+
+#Calculate Percentage for each Candidate
+KhanPercent = str(round(((KhanVotes/(CountofVotes))*100.00), 2))
+CorreyPercent = str(round(((CorreyVotes/(CountofVotes))*100.00), 2))
+LiPercent = str(round(((LiVotes/(CountofVotes))*100.00), 2))
+OTooleyPercent = str(round(((OTooleyVotes/(CountofVotes))*100.00), 2))
+
+
+
+
+
+
+######################
+# Print To Terminal
+######################
+#Print Variables to Terminal 
+print(f"------------------------------")
+print(f"Total of Votes: {CountofVotes}")
+print(f"------------------------------")
+print (f"Khan: {KhanPercent}%     Votes:  {KhanVotes}")
+print (f"Correy: {CorreyPercent}%   Votes:  {CorreyVotes}")
+print (f"Li: {LiPercent}%       Votes:  {LiVotes}")
+print (f"O'Tooley: {OTooleyPercent}%  Votes:  {OTooleyVotes}")
+# print(" ")
+# print(" ")
+print(f"------------------------------")
+print(f"Winner: {CountofVotes}")
+print(f"------------------------------")
+
+
+######################
+# Generate Text File
+######################
+
+#Setting Filepath manually due to issues 
+#print(os.path.abspath(os.getcwd()))
+currentpath=(os.path.abspath(os.getcwd()))
+resultfilepath=(currentpath+'\Analysis\Results.txt')
+print(f"Writing file to following location: {resultfilepath} ")
+
+f = open(resultfilepath, 'w')
+f.write(f"------------------------------")
+f.write('\n')
+f.write(f"Total of Votes: {CountofVotes}")
+f.write('\n')
+f.write(f"------------------------------")
+f.write('\n')
+f.write('\n')
+f.write(f"Khan: {KhanPercent}%    Votes:  {KhanVotes}")
+f.write('\n')
+f.write(f"Correy: {CorreyPercent}%  Votes:  {CorreyVotes}")
+f.write('\n')
+f.write(f"Li: {LiPercent}%      Votes:  {LiVotes}")
+f.write('\n')
+f.write(f"O'Tooley: {OTooleyPercent}%  Votes:  {OTooleyVotes}")
+f.write('\n')
+f.write(f"------------------------------")
+f.write('\n')
+f.write(f"Winner: {winner}")
+f.write('\n')
+f.write(f"------------------------------")
+f.write('\n')
+f.close()
